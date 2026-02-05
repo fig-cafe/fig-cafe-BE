b06424f0229b42e530d5d79adc7e6477cfe44104
seongjinYU <128691265+seongjinYU@users.noreply.github.com>
2026-02-05 17:48:23 +0900

    Chore: NestJS 초기 세팅 및 PostgreSQL 연결
    - NestJS 프로젝트 뼈대 생성
    - TypeORM, pg, @nestjs/config 설치
    - app.module.ts에 DB 연결 설정 추가

---
+++ src/app.module.ts
@@ -0,0 +1,30 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true, // 개발 환경에서만 사용 (운영에서는 false)
+      }),
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
